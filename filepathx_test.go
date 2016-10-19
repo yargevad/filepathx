392836c94a26f17b84affa90f7ac139d0511d01f
Dave Gray <yargevad+github@gmail.com>
2016-10-19 09:25:34 -0600

    remove prefixes from test errors; test nil input to Expand

--- filepathx_test.go
+++ filepathx_test.go
@@ -10,18 +10,18 @@ func TestGlob_ZeroDoubleStars_oneMatch(t *testing.T) {
 	path := "./a/b/c.d/e.f"
 	err := os.MkdirAll(path, 0755)
 	if err != nil {
-		t.Fatalf("ZeroDoubleStars: os.MkdirAll: %s", err)
+		t.Fatalf("os.MkdirAll: %s", err)
 	}
 	matches, err := Glob("./*/*/*.d")
 	if err != nil {
-		t.Fatalf("ZeroDoubleStars: Glob: %s", err)
+		t.Fatalf("Glob: %s", err)
 	}
 	if len(matches) != 1 {
-		t.Fatalf("ZeroDoubleStars: got %d matches, expected 1", len(matches))
+		t.Fatalf("got %d matches, expected 1", len(matches))
 	}
 	expected := "a/b/c.d"
 	if matches[0] != expected {
-		t.Fatalf("ZeroDoubleStars: matched [%s], expected [%s]", matches[0], expected)
+		t.Fatalf("matched [%s], expected [%s]", matches[0], expected)
 	}
 }
 
@@ -30,18 +30,18 @@ func TestGlob_OneDoubleStar_oneMatch(t *testing.T) {
 	path := "./a/b/c.d/e.f"
 	err := os.MkdirAll(path, 0755)
 	if err != nil {
-		t.Fatalf("OneDoubleStar: os.MkdirAll: %s", err)
+		t.Fatalf("os.MkdirAll: %s", err)
 	}
 	matches, err := Glob("./**/*.f")
 	if err != nil {
-		t.Fatalf("OneDoubleStar: Glob: %s", err)
+		t.Fatalf("Glob: %s", err)
 	}
 	if len(matches) != 1 {
-		t.Fatalf("OneDoubleStar: got %d matches, expected 1", len(matches))
+		t.Fatalf("got %d matches, expected 1", len(matches))
 	}
 	expected := "a/b/c.d/e.f"
 	if matches[0] != expected {
-		t.Fatalf("OneDoubleStar: matched [%s], expected [%s]", matches[0], expected)
+		t.Fatalf("matched [%s], expected [%s]", matches[0], expected)
 	}
 }
 
@@ -50,19 +50,19 @@ func TestGlob_OneDoubleStar_twoMatches(t *testing.T) {
 	path := "./a/b/c.d/e.f"
 	err := os.MkdirAll(path, 0755)
 	if err != nil {
-		t.Fatalf("OneDoubleStar: os.MkdirAll: %s", err)
+		t.Fatalf("os.MkdirAll: %s", err)
 	}
 	matches, err := Glob("./a/**/*.*")
 	if err != nil {
-		t.Fatalf("OneDoubleStar: Glob: %s", err)
+		t.Fatalf("Glob: %s", err)
 	}
 	if len(matches) != 2 {
-		t.Fatalf("OneDoubleStar: got %d matches, expected 2", len(matches))
+		t.Fatalf("got %d matches, expected 2", len(matches))
 	}
 	expected := []string{"a/b/c.d", "a/b/c.d/e.f"}
 	for i, match := range matches {
 		if match != expected[i] {
-			t.Fatalf("OneDoubleStar: matched [%s], expected [%s]", match, expected[i])
+			t.Fatalf("matched [%s], expected [%s]", match, expected[i])
 		}
 	}
 }
@@ -72,17 +72,28 @@ func TestGlob_TwoDoubleStars_oneMatch(t *testing.T) {
 	path := "./a/b/c.d/e.f"
 	err := os.MkdirAll(path, 0755)
 	if err != nil {
-		t.Fatalf("TwoDoubleStars: os.MkdirAll: %s", err)
+		t.Fatalf("os.MkdirAll: %s", err)
 	}
 	matches, err := Glob("./**/b/**/*.f")
 	if err != nil {
-		t.Fatalf("TwoDoubleStars: Glob: %s", err)
+		t.Fatalf("Glob: %s", err)
 	}
 	if len(matches) != 1 {
-		t.Fatalf("TwoDoubleStars: got %d matches, expected 1", len(matches))
+		t.Fatalf("got %d matches, expected 1", len(matches))
 	}
 	expected := "a/b/c.d/e.f"
 	if matches[0] != expected {
-		t.Fatalf("TwoDoubleStars: matched [%s], expected [%s]", matches[0], expected)
+		t.Fatalf("matched [%s], expected [%s]", matches[0], expected)
+	}
+}
+
+func TestExpand_DirectCall_emptySlice(t *testing.T) {
+	var empty []string
+	matches, err := Globs(empty).Expand()
+	if err != nil {
+		t.Fatalf("Glob: %s", err)
+	}
+	if len(matches) != 0 {
+		t.Fatalf("got %d matches, expected 0", len(matches))
 	}
 }
